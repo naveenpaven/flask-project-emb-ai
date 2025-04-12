5c933673e62d22dda7a016262b20559829b8e688
Naveen Pandu <naveen.paven@gmail.com>
2025-04-12 09:45:40 -0400

    dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,10 +1,23 @@
 import json
 import requests
 
+def get_emotion(dict, p_value):
+    for key,value in dict.items():
+        if value == p_value:
+            return key
+    return none
+
 def emotion_detector(text_to_analyze):
     URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     input = { "raw_document": { "text": text_to_analyze } }
     response = requests.post(URL,json = input, headers=header)
 
-    return response.text
+    response_json = json.loads(response.text)
+    emotion = response_json["emotionPredictions"][0]["emotion"]
+    
+    highest_emotion_value = max(emotion.values())
+    dominant_emotion = get_emotion(emotion,highest_emotion_value)
+    emotion['dominant_emotion'] = dominant_emotion
+
+    return emotion
