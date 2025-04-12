921fe3dca541f6d1cf5b92059ef1137653b18af4
Naveen Pandu <naveen.paven@gmail.com>
2025-04-12 11:17:40 -0400

    web app

---
+++ server.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detection")
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def emotion_detect():
+    text_to_analyze = request.args.get('textToAnalyze')
+    print(text_to_analyze)
+    response = emotion_detector(text_to_analyze)
+
+    output = ""
+    rowcount = 1
+    for key,value in response.items():
+        if key != "dominant_emotion" and rowcount != len(response) -1:
+            output = output + "'" + key + "': " + str(value) + ", "
+
+        if rowcount == len(response) -1:
+            output = output + "and '" + key + "': " + str(value) + ". "
+
+        if key == "dominant_emotion":
+            output = output + "The dominant emotion is " + value + "."
+    
+        rowcount += 1
+
+    return f"For the given statement, the system response is  {output}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
