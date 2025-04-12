adeeb6718d4353aa4b2382ad76a60adc14dd798a
Naveen Pandu <naveen.paven@gmail.com>
2025-04-12 09:04:45 -0400

    Initial Commit

---
+++ emotion_detection.py
@@ -0,0 +1,10 @@
+import json
+import requests
+
+def emotion_detector(text_to_analyze):
+    URL = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input = { "raw_document": { "text": text_to_analyze } }
+    response = requests.post(URL,json = input, headers=header)
+
+    return response.text
